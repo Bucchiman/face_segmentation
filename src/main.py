4e52877cbe886d06a067c9a5d282823d5bb53a83
Yuki Iwabuchi <iwabuchi@fun.bio.keio.ac.jp>
2021-09-17 17:36:42 +0900

    distributed training initial_group

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@
 #
 # FileName: 	main
 # CreatedDate:  2021-09-15 04:09:09 +0900
-# LastModified: 2021-09-17 17:23:51 +0900
+# LastModified: 2021-09-17 17:35:56 +0900
 #
 
 
@@ -12,7 +12,9 @@ import sys
 from pathlib import Path
 import cv2
 import numpy as np
+import torch
 from torch import nn
+from torch import distributed as dist
 from torch.utils.data import DataLoader, DistributedSampler
 from utils.myparse import get_args
 from models.model import BiSeNet
@@ -28,6 +30,11 @@ def main():
     # 5 mouth
 
     args = get_args()
+
+    dist.init_process_group(backend='nccl',
+                            init_method='tcp://127.0.0.1:33241',
+                            world_size=torch.cuda.device_count(),
+                            rank=-1)
 
     resize_path = os.path.join(args["output_path"], "resize")
     mask_path = os.path.join(args["output_path"], "mask")
